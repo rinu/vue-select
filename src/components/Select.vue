d69d8bfd8a39e1a321bf27c42511d4a9d7746b17
Rauno Moisto <rauno.moisto@credy.com>
2019-04-18 17:58:15 +0300

    fixed search on android

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -340,7 +340,7 @@
 
       <input
         ref="search"
-        v-model="search"
+        :value="search"
         @keydown.delete="maybeDeleteValue"
         @keyup.esc="onEscape"
         @keydown.up.prevent="typeAheadUp"
@@ -348,6 +348,7 @@
         @keydown.enter.prevent="typeAheadSelect"
         @blur="onSearchBlur"
         @focus="onSearchFocus"
+        @input="search = $event.target.value"
         type="search"
         class="form-control"
         :class="inputClasses"
